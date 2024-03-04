4db9f6c6c3d11e2ba732d304a7502d34e39c3365
syafiinur02 <syafiinur02@gmail.com>
2024-03-04 18:35:47 +0700

    update /youtube

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/testing', (req, res) => {
     res.send(`Hello, Testing! The current time is ${new Date().toLocaleTimeString()}.`);
   });
 
+app.get('/youtube', (req, res) => {
+res.send(`Hello, Testing! The current time is ${new Date().toLocaleTimeString()}.`);
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
