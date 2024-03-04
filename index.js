7cdede80770a0018906a23ed80a82f8ccc13310a
syafiinur02 <syafiinur02@gmail.com>
2024-03-04 18:30:33 +0700

    delete endpoint all

--- index.js
+++ index.js
@@ -7,22 +7,6 @@ app.get('/', (req, res) => {
   res.send(`Hello, World! The current time is ${new Date().toLocaleTimeString()}.`);
 });
 
-app.get('/api', (req, res) => {
-    res.send(`Ini adalah api dengan jam sekarang ${new Date().toLocaleTimeString()}.`);
-});
-
-app.get('/dashboard', (req, res) => {
-    res.send(`Ini adalah dashboard dengan jam sekarang ${new Date().toLocaleTimeString()}.`);
-});
-
-app.get('/admin', (req, res) => {
-    res.send(`Ini adalah admin dengan jam sekarang ${new Date().toLocaleTimeString()}.`);
-});
-
-app.get('/testing', (req, res) => {
-    res.send(`Ini adalah admin dengan jam sekarang ${new Date().toLocaleTimeString()}.`);
-});
-
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
