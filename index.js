70f47a0167fa88bbd13ecb70ec3e4f5c0457ea23
syafiinur02 <syafiinur02@gmail.com>
2024-03-04 18:32:36 +0700

    update /testing

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send(`Hello, World! The current time is ${new Date().toLocaleTimeString()}.`);
 });
 
+app.get('/testing', (req, res) => {
+    res.send(`Hello, Testing! The current time is ${new Date().toLocaleTimeString()}.`);
+  });
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
